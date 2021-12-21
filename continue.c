3ee174e7f6073aae2ffc38efdcdef7145ba1f07c
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-21 20:45:12 +0530

    Update continue.c

--- continue.c
+++ continue.c
@@ -1,24 +1,21 @@
+/*do-while, if-else, continue*/
 #include<stdio.h>
-
 int main()
 {
     int i=0;
     int n;
-    printf("enter a number = \n");
+    printf("Enter a number = \n");
     scanf("%d",&n);
     do
     {
-        printf("value of i is %d \n", i++);
+        printf("Value of i is %d \n", i++);
         if (i!=5)
         {
             continue;
         }
         else{
             printf("%d \n",i);
-        }
-        
-    } while (i<n);
-    
-    
+        }   
+    } while (i<n);    
     return 0;
 }
