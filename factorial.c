093d46aa6b8471d701de5745c94fab9f176cce6b
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-21 21:04:11 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,13 +1,13 @@
+/*factorial using for-loop*/
 #include<stdio.h>
-
 int main(){
     int i=0,n,fac=1;
-    printf("enter a number = ");
+    printf("Enter a number = ");
     scanf("%d", &n);
     for (i = 1; i <= n ; i++)
     {
         fac*=i;
     }
-    printf("the value of factorial %d is %d",n,fac);
+    printf("Factorial of %d is %d",n,fac);
     return 0;
 }
