292ac8d47cdf0e35644027685a17b6407e985e79
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-21 21:01:44 +0530

    Update doWhile.c

--- doWhile.c
+++ doWhile.c
@@ -1,21 +1,18 @@
+/*do-while, break*/
 #include<stdio.h>
-
 int main()
 {
     int i=0;
     int n;
-    printf("enter a number = \n");
+    printf("Enter a number = \n");
     scanf("%d",&n);
     do
     {
-        printf("value of i is %d \n", i++);
+        printf("Value of i is %d \n", i++);
         if (i==50)
         {
             break;
-        }
-        
-    } while (i<n);
-    
-    
+        }      
+    } while (i<n+1);    
     return 0;
 }
