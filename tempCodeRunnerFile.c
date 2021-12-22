327eeada1bd575ddfe4c18885cb5fecffd914bb3
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-22 20:21:37 +0530

    Update tempCodeRunnerFile.c

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,24 +1,24 @@
+//printing multiplication table
 #include <stdio.h>
 #include <stdlib.h>
 int main()
 {
-   int *ptr;
+   int *ptr, no;
    ptr = (int *)malloc(10 * sizeof(int));
-
+   
+   printf("Enter a number: ");
+   scanf("%d",&no);
    for (int i = 0; i < 10; i++)
    {
-      ptr[i] = 7 * (i + 1);
-      printf("7 X %d : %d\n", i + 1, ptr[i]);
+      ptr[i] = no * (i + 1);
+      printf("%d X %d : %d\n",no, i + 1, ptr[i]);
    }
    printf("\n..........................................\n \n");
-
    ptr = realloc(ptr, 15 * sizeof(int));
-
    for (int i = 0; i < 15; i++)
    {
-      ptr[i] = 7 * (i + 1);
-      printf("7 X %d : %d\n", i + 1, ptr[i]);
+      ptr[i] = no * (i + 1);
+      printf("%d X %d : %d\n",no, i + 1, ptr[i]);
    }
-
    return 0;
 }
