12020fb987b0c64004a377418499928c008a7aeb
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-22 20:54:10 +0530

    Update grade.c

--- grade.c
+++ grade.c
@@ -1,22 +1,29 @@
+//ranging in case
 #include<stdio.h>
-
 int main(){
     int marks;
-    printf("enter the marks ");
-    scanf("%d",marks);
+    printf("Enter the marks ");
+    scanf("%d",&marks);
     switch (marks)
     {
-    case 90-100:
-       printf("grade is A");
-        // break;
-    case 80-89:
-       printf("grade is B");
+    case 90 ... 100:
+       printf("Grade is A");
+         break;
+    case 80 ... 89:
+       printf("Grade is B");
         break;
-        
+    case 70 ... 79:
+       printf("Grade is C");
+        break;
+    case 60 ... 69:
+       printf("Grade is D");
+        break;
+    case 50 ... 59:
+       printf("Grade is E");
+        break;
     default:
-        printf("grade is F");
+        printf("Grade is F");
         break;
     }
-
     return 0;
 }
