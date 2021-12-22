13bc51bedff242a246d7078982b7f8325ec33917
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-22 20:27:22 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,8 +1,9 @@
+//Sum of numbers from 1 to n (given number)
 #include<stdio.h>
 int main()
 {
     int i=1,sum=1, n;
-    printf("enter a number = ");
+    printf("Enter a number = ");
     scanf("%d",&n);
     // for ( i = 0; i <= n; i++)
     // {
