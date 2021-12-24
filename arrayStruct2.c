9fb44e7af82e0969d481c35a08a0d48623b8ce54
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-24 13:23:44 +0530

    Update arrayStruct2.c

--- arrayStruct2.c
+++ arrayStruct2.c
@@ -1,11 +1,11 @@
+// structure, pointer and loop
 #include <stdio.h>
 #include <string.h>
-
 struct employee
 {
     int code;
     float salary;
-    char name[100];
+    char name[10];
 };
 int main()
 {
@@ -15,18 +15,15 @@ int main()
     for (int i = 1; i <= n; i++)
     {
         struct employee e[n];
-
+        struct employee *ptr;
+        ptr=e;
         printf("Enter the value of code for employee %d : ", i);
         scanf("%d", &e[n].code);
-
         printf("Enter the value of salary for employee %d : ", i);
         scanf("%f", &e[n].salary);
-
         printf("Enter the name of employee %d : ", i);
         scanf("%s", e[n].name);
-        
         printf(" %d %.2f %s \n", e[n].code, e[n].salary, e[n].name);
     }
-
     return 0;
 }
