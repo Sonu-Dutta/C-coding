e4fdd6ac652f2bd8a33c3ae55413c086c940374b
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-24 12:30:47 +0530

    Update arrayFunction.c

--- arrayFunction.c
+++ arrayFunction.c
@@ -1,26 +1,29 @@
+// array example: Passing array as parameter to a function. 
 #include<stdio.h>
+// method-1
+
 // void print_array(int *ptr, int n)
 // {
 //     for (int i = 0; i < n; i++)
 //     {
-//         printf("the value of element %d is %d \n",i+1,*(ptr+i));
+//         printf("The value of element %d is %d \n",i+1,*(ptr+i));
 //     }
-    
+//         ptr[2]=95; //this value will change in main
 // }
 
-// or you can do in this way
+//method-2
 
 void print_array(int ptr[], int n)
 {
     for (int i = 0; i < n; i++)
     {
-        printf("the value of element %d is %d \n",i+1,ptr[i]);
+        printf("The value of element %d is %d \n",i+1,ptr[i]);
     }
     ptr[2]=95; //this value will change in main
 }
 
 int main(){
-    int arr[]={21,37,84,75,75,23,20};
+    int arr[]={18,57,94,92,85,29,20};
     print_array(arr,7);
     printf("%d",arr[2]);
     return 0;
