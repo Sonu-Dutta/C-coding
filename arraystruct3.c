80ff3864e338c740d824ce9db8c35c3f4f26be00
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-24 13:27:44 +0530

    Update arraystruct3.c

--- arraystruct3.c
+++ arraystruct3.c
@@ -1,17 +1,15 @@
+// Structure example:
 #include <stdio.h>
 #include <string.h>
-
 struct employee
 {
-    int code;
+    int id;
     float salary;
     char name[100];
 };
 int main()
 {
-
-    struct employee e={100,345500,"sanjay"};
-    printf(" code: %d \n salary: %.2f per month \n name: %s", e.code, e.salary, e.name);
-
+    struct employee e={10,950000,"Sonu Dutta"};
+    printf(" Id: %d \n Salary: %.2f per month \n Name: %s", e.id, e.salary, e.name);
     return 0;
 }
