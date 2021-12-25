f91c3519e87991f065691694edc44c498f5cdbaa
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-25 14:14:36 +0530

    Update occurence.c

--- occurence.c
+++ occurence.c
@@ -1,28 +1,23 @@
+// string example:
 #include <stdio.h>
 int occurence(char st[], char c)
 {
     char *ptr = st;
     int count = 0; 
-    // while (*ptr != '\0')
-    // {
+    while (*ptr != '\0')
+    {
         if (*ptr == c)
         {
-            // count++;
-                printf("The given value is present \n");
+           count++;
         }
-        // ptr++;
-        else{
-            printf("The given value is not present \n");
-        }
-    // }
-
+         ptr++;
+    }
     return count;
 }
-
 int main()
 {
     char st[] = "sooonuu";
-    int count = occurence(st, 'a');
-    // printf("occurence = %d", count);
+    int count = occurence(st, 'u');
+ printf("occurence = %d", count);
     return 0;
 }
