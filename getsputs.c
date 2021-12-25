120ed3a2dbeb3a6905dcf945f786970c578df216
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-25 13:43:22 +0530

    Update getsputs.c

--- getsputs.c
+++ getsputs.c
@@ -1,11 +1,10 @@
+// gets-puts example:
 #include<stdio.h>
-
 int main(){
     char name[30];
     printf("Enter your name : ");
     gets(name);
     // printf("Your name is %s",name);
     puts(name);
-
     return 0;
 }
