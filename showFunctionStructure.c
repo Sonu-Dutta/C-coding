9dfa1206c33d1938efbc0763dd3dea2d64e1130f
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-26 17:18:50 +0530

    Update showFunctionStructure.c

--- showFunctionStructure.c
+++ showFunctionStructure.c
@@ -1,6 +1,6 @@
+//Function + Structure + Pointer example:
 #include <stdio.h>
 #include <string.h>
-
 struct Employee
 {
     int code;
@@ -9,18 +9,19 @@ struct Employee
 };
 void show(struct Employee emp)
 {
+    emp.code = 1002;
     printf("The code of employee is : %d\n", emp.code);
     printf("The salary of employee is : %.2f\n", emp.salary);
     printf("The name of employee is : %s\n", emp.name);
-    emp.code = 34;
+    emp.code = 1003;
 }
 int main()
 {
     struct Employee e;
     struct Employee *ptr;
     ptr = &e;
-    ptr->code = 101;
-    ptr->salary = 109999;
+    ptr->code = 1001;
+    ptr->salary = 9999000.8765;
     strcpy(ptr->name, "Vaishakhi");
     printf("The code of employee is : %d\n", e.code);
     show(e);
