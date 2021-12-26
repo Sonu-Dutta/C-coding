446c3f1dc87b3121681f99f817d79c75ee42497f
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-26 17:23:51 +0530

    Update string.c

--- string.c
+++ string.c
@@ -1,11 +1,12 @@
+//string example:
 #include<stdio.h>
 #include<string.h>
 
 int main(){
-    char str[]={'S','o','n','u','d','u','t','t','a', '\0'};//0 is null character
-    int a =strlen(str);
+    char str[]={'S','o','n','u',' ','d','u','t','t','a', '\0'};//0 is null character
     printf("%s\n",str);
+    
+    int a =strlen(str);
     printf("the length of string is %d ",a);
-    // printf("%s",str2);
     return 0;
 }
