2db998e253a870d18dfe349c6583638f37bfe989
Sonu-Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2021-12-26 16:59:45 +0530

    Update pointer2.c

--- pointer2.c
+++ pointer2.c
@@ -1,9 +1,9 @@
+//pointer to pointer example:
 #include<stdio.h>
-
 int main(){
-    int i=4;
-    int *ptr=&i;
-    int **ptr_ptr=&ptr;
-    printf("Value of i %d",**ptr_ptr);
+    int i=9;
+    int *p=&i;
+    int **q=&p;
+    printf("Value of i is %d",**q);
     return 0;
 }
